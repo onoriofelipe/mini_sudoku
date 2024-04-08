b23524131fe9f7f014031a604680e056db0eb437
onorio <onoriofelipe7@gmail.com>
2024-04-07 23:48:15 -0300

    Add decreasing mana mechanic

--- microjogos/2024S1/projeto_felipe_onorio/cenas/mana.gd
+++ microjogos/2024S1/projeto_felipe_onorio/cenas/mana.gd
@@ -3,8 +3,14 @@ extends TextureProgressBar
 func _ready():
 	set_fill_mode(3)
 
+var accumulated_deltas = 0
 
 func _process(delta):
+	accumulated_deltas += delta
+	if(accumulated_deltas >= 0.1):
+		accumulated_deltas -= 0.1
+		value -= 1.5
+		value = max(value, 0)
 	pass
 
 func _on_mana_gained(amount: int):
